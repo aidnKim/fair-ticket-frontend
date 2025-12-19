77b3ffa9a00dbc3cbc408a2ccb8f86ce43ccfcac
kimhyeonsoo <hyeonsookim2@gmail.com>
2025-12-19 21:03:31 +0900

    기본 설정 완료

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
+// src/main.jsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+//SPA 를 구현하기 위한 RouterProvider 를 import
+import { RouterProvider } from 'react-router-dom'
+// routing 정보를 담고 있는 router import
+import router from './router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
