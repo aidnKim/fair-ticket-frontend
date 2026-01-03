8f708a1eec2ec9aa6b5e8f57de39c6f7ac8952f1
kimhyeonsoo <hyeonsookim2@gmail.com>
2026-01-03 21:59:20 +0900

    Feat: concert detail 페이지 구현 완료

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -48,8 +48,8 @@ const Header = () => {
             onClick={handleSearch}
             className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
             </svg>
 
           </button>
@@ -69,7 +69,7 @@ const Header = () => {
             <li className={`cursor-pointer ${location.pathname === '/' ? 'text-red-600 border-b-2 border-red-600 pb-1' : 'hover:text-red-600'}`}>
               <Link to="/">홈</Link>
             </li>
-            <li className={`cursor-pointer ${location.pathname === '/concert' ? 'text-red-600 border-b-2 border-red-600 pb-1' : 'hover:text-red-600'}`}>
+            <li className={`cursor-pointer ${location.pathname.startsWith('/concert') ? 'text-red-600 border-b-2 border-red-600 pb-1' : 'hover:text-red-600'}`}>
               <Link to="/concert">콘서트</Link>
             </li>
           </ul>
